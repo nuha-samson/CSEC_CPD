1a8f09995e2ea314942e8b0f41b6a66b680455f2
Nuha Samson <nuhaskingdom@gmail.com>
2026-08-21 22:13:59 +0300

    Update Team.py

--- Team.py
+++ Team.py
@@ -2,8 +2,11 @@
 n = int(input())
 count = 0
 for i in range(n):
-    lst = list(map(int, input().split()))
-    count1 = sum(1 for i in lst if  i == 1)
-    if count1 >= 2:
+    lst = list(map(int,input().split()))
+    add = sum(lst)
+    if add >= 2:
         count += 1
 print(count)
+    
+    
+    
