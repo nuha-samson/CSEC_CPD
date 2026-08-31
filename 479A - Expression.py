2dabef940b1c24fb5f9251ec319bc5b8a2bc7d80
Nuha Samson <nuhaskingdom@gmail.com>
2026-08-31 12:27:47 +0300

    Create 479A - Expression.py

---
+++ 479A - Expression.py
@@ -0,0 +1,7 @@
+#cook your dish here
+a, b, c = int(input()),int(input()),int(input())
+ans = a + b + c
+ans = max(ans, (a + b) * c)
+ans = max(ans, a * (b + c))
+ans = max(ans, a * b * c)
+print(ans)
